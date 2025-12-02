164ef7f7ae7d000d4dc4cadbf44f409aab7838ff
jaybrar23 <57345162+jaybrar23@users.noreply.github.com>
2025-12-02 20:18:50 +1100

    day 1 done

--- 2025-12-01/solution_20251201_part2.py
+++ 2025-12-01/solution_20251201_part2.py
@@ -1,22 +1,24 @@
 #dial numbers 0 - 99
 #starts at 50
 
-def Left_Turn(dial,move):
+def Left_Turn(dial,move,count):
     if int(move[1:]) <= dial:
-        return dial-(int(move[1:]))
+        return dial-(int(move[1:])), count
     else:
         move = "L" + str(int(move[1:])-dial)
+        if dial!=0: count = count + 1
         dial = 100
-        return Left_Turn(dial,move)
+        return Left_Turn(dial,move,count)
 
 
-def Right_Turn(dial,move):
+def Right_Turn(dial,move,count):
     if int(move[1:]) <= (99-dial):
-        return dial+(int(move[1:]))
+        return dial+(int(move[1:])),count
     else:
         move = "R" + str(int(move[1:])-(100-dial))
+        if int(move[1:]) !=0: count = count + 1
         dial = 0
-        return Right_Turn(dial,move)
+        return Right_Turn(dial,move,count)
     
 f = open("C:\\Users\\Zoop\\Desktop\\SDE Learning\\git\\adventofcode\\2025-12-01\\Input.txt")
 dial = 50
@@ -24,11 +26,10 @@ count = 0
 for line in f:
     line = line.strip()
     if line[0:1]=="L":
-        dial = Left_Turn(dial,line)
+        dial,count = Left_Turn(dial,line,count)
     if line[0:1]=="R":
-        dial = Right_Turn(dial,line)
+        dial,count = Right_Turn(dial,line,count)
     if(dial==0): count = count+1
-    print(line + " - " + str(dial) + " - " + str(count))
 print(count)
 
 #right turning to 0 instead of 99 
