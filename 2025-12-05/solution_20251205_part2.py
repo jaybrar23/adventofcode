014ca67456bf7aee675383c0fbbfd7b5f4427719
jaybrar23 <57345162+jaybrar23@users.noreply.github.com>
2025-12-16 18:58:21 +1100

    day5 completed

--- 2025-12-05/solution_20251205_part2.py
+++ 2025-12-05/solution_20251205_part2.py
@@ -1,4 +1,4 @@
-f = open("C:\\Users\\Zoop\\Desktop\\SDE Learning\\git\\adventofcode\\2025-12-05\\input_sample2.txt")
+f = open("C:\\Users\\Zoop\\Desktop\\SDE Learning\\git\\adventofcode\\2025-12-05\\input.txt")
 
 def Check_Freshness(fresh_range,ingredient):
     range_start,range_finish = fresh_range.split("-")
@@ -13,22 +13,17 @@ def Check_Indvidual_Range(r,fresh_ranges):
     rs = int(rs)
     rf = int(rf)
     for r2 in fresh_ranges:
-        print(r+" <>" + r2)
         if r == r2: 
-            print("continue")
             continue
         elif (Check_Freshness(r2,rs) and Check_Freshness(r2,rf)):
             return False
         elif Check_Freshness(r2,rs):
-            print("start is removed")
             #if start falls into any range
             rs = r2.split("-")[0]
         elif Check_Freshness(r2,rf):
-            print("end is removed")
             #if end falls into any range
             rf = r2.split("-")[1]
     new_r = str(rs)+"-"+str(rf)
-    print(new_r+" is added")
     return new_r
 
 
@@ -42,6 +37,8 @@ def main(fresh_ranges):
             new_range.append(new_r)
     return new_range
 
+
+#sort out the input
 arr = []
 for line in f:
     if line.strip() == "":
@@ -51,19 +48,16 @@ for line in f:
     arr.append(line.strip())
 ingredients = arr
 
-for i in range(1):
-    fresh_ranges = (main(fresh_ranges))
+fresh_ranges = list(set(fresh_ranges))
 
+for i in range(10):
+    fresh_ranges = (main(fresh_ranges))
 
 count = 0
 fresh_ranges.sort()
-print(fresh_ranges)
 for r in fresh_ranges:
     if r is False: continue
     rs,rf = r.split("-")
     s = int(rf)-int(rs)+1
     count = count + s
-    print(r+" - > \t" + str(s))
 print(count)
-
-#421073057078519
